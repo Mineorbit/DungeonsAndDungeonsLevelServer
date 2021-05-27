6742478ac094e88691f20ef8f5f28e1f7b38b4c7
mineorbit <mineorbitbeta@gmail.com>
2021-05-27 17:33:32 +0200

    fix

--- backend/users/create/views.py
+++ backend/users/create/views.py
@@ -1,16 +1,37 @@
 from flask import Blueprint, current_app
+from flask_pydantic import validate
+from pydantic import BaseModel
 
 from users.models import User, db
 
 users_create_app = Blueprint('users_create',__name__)
 
+class CreateUser(BaseModel):
+    name: str
+    password: str
 
-@users_create_app.route("/users")
-def create_user():
+    class Config:
+        orm_mode = True
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+
+@users_create_app.route('/users', methods=['POST'])
+def register(create: CreateUser):
     user = User(
-        username = "Test"
-    )
-    user.set_password("Testpasswort")
-    db.session.add(user)
-    db.session.commit()
-    return "New User "+user.id.__str__()
+        username=create.name)
+    user.set_password(create.password)
+    return UserOut.from_orm(user)
+
+
+@users_create_app.route('/users/', methods=['GET'])
+def read():
+    c = CreateUser(name = "Max",
+                   password = "Test")
+    return CreateUser.from_orm(c)
