216016eb98f11685dfe2be6c615233b6a4b542ee
mineorbit <mineorbitbeta@gmail.com>
2021-05-29 20:48:43 +0200

    added frontend read support

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from base.routes import router as baseRouter
 from user.routes import router as userRouter
@@ -11,6 +12,21 @@ from util import engine, Base
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(engine)
 
