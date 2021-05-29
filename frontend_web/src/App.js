a20f48049224d67f97ffe68d99a87a1f3201cd1b
mineorbit <mineorbitbeta@gmail.com>
2021-05-29 22:44:51 +0200

    started work on frontend

--- frontend_web/src/App.js
+++ frontend_web/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, {Component} from "react";
 
-function App() {
+class App extends Component {
+    state = {levels: []}
 
-  const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("http://localhost:8000/")
-      .then((res) => res.json())
-      .then((data) => {console.log("Received: "+data);setData(data.message);});
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+    componentDidMount() {
+        fetch("http://localhost:8000/level/all")
+            .then((res) => res.json())
+            .then((levels) => {
+                console.log(JSON.stringify(levels, null, 2) +" "+levels[0].name);
+                this.setState({levels});
+                console.log(this.state);
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <table>{ this.state.levels.map(level => <tr><td>{level.ulid}</td><td>{level.name}</td><td>{level.description}</td><td>{level.upload_date}</td><td><a href={"http://localhost:8000/level/download/?ulid="+level.ulid}>Download</a> </td></tr>)}</table>
+                </header>
+               </div>);
+
+    }
 }
-
 export default App;
