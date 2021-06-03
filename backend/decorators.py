681c3a2377eff852e7c438f819ae01054ab787e8
mineorbit <mineorbitbeta@gmail.com>
2021-06-04 00:26:07 +0200

    added media type to protobuf transmission

--- backend/decorators.py
+++ backend/decorators.py
@@ -1,13 +1,13 @@
 from merge_args import merge_args
 from google.protobuf.json_format import MessageToJson
-
+from fastapi import Response
 def proto_resp(f):
     @merge_args(f)
     async def wrapped(proto_resp: bool, *args, **kwargs):
 
         r = await f(*args, **kwargs)
         if proto_resp:
-            return MessageToJson(r.to_proto())
+            return Response(content=r.to_proto().SerializeToString(), media_type="application/octet-stream")
         else:
             return r
 
