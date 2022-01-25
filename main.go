1a6374219adf21c37fa579e51f845cff9dfdef22
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-01-26 00:40:45 +0100

    change

--- main.go
+++ main.go
@@ -57,15 +57,15 @@ func Login(w http.ResponseWriter, r *http.Request) {
   u.Password = r.Form.Get("password")
   //compare the user from the request, with the one we defined:
   if user.Username != u.Username || user.Password != u.Password {
-  fmt.Fprintf(w, "Login Wrong, %q", html.EscapeString(r.URL.Path))
+  fmt.Fprintf(w, "Login Wrong")
      return
   }
   token, err := CreateToken(user.ID)
   if err != nil {
-     c.JSON(http.StatusUnprocessableEntity, err.Error())
+     fmt.Fprintf(w, err.Error())
      return
   }
-  fmt.Fprintf(w, token, html.EscapeString(r.URL.Path))
+  fmt.Fprintf(w, token)
 }
 
 
