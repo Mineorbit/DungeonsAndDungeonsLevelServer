bee7d52629d7fd1f72f32d706a9e72b69e2c2b7d
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-01-26 22:11:22 +0100

    started work on level server handle

--- main.go
+++ main.go
@@ -13,22 +13,9 @@ import (
     "io/ioutil"
   "os"
   "time"
+  "DungeonsAndDungeonsLevelServer"
 )
 
-// album represents data about a record album.
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
 
 const (
   host     = "localhost"
@@ -88,12 +75,23 @@ func CreateToken(userId uint64) (string, error) {
 }
 
 
-
-
-
-func uploadFile(w http.ResponseWriter, r *http.Request) {
+func handleLevel(w http.ResponseWriter, r *http.Request){
 
 	proto_resp := r.URL.Query()["proto_resp"]
+	switch r.Method {
+	case "GET":
+		getLevelList(w,r,proto_resp)
+	case "POST":
+		uploadFile(w,r,proto_resp)
+	}
+}
+
+func getLevelList(w http.ResponseWriter, r *http.Request, proto_resp bool){
+    fmt.Fprintf(w, "LevelList\n")
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request, proto_resp bool) {
+
 	description := r.URL.Query()["description"]
 	
     // Parse our multipart form, 10 << 20 specifies a maximum
@@ -131,7 +129,7 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func setupRoutes() {
-    http.HandleFunc("/level/", uploadFile)
+    http.HandleFunc("/level/", handleLevel)
 	http.HandleFunc("/auth/token",Login)
     http.ListenAndServe(":8080", nil)
 }
