6ba7a52b8fa1ba683593bffaa7a7d492043a6b0c
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-01-26 01:09:10 +0100

    fix

--- main.go
+++ main.go
@@ -92,15 +92,17 @@ func CreateToken(userId uint64) (string, error) {
 
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("File Upload Endpoint Hit")
 
+	proto_resp = r.URL.Query()["proto_resp"]
+	description = r.URL.Query()["description"]
+	
     // Parse our multipart form, 10 << 20 specifies a maximum
     // upload of 10 MB files.
     r.ParseMultipartForm(10 << 20)
     // FormFile returns the first file for the given key `myFile`
     // it also returns the FileHeader so we can get the Filename,
     // the Header and the size of the file
-    file, handler, err := r.FormFile("myFile")
+    file, handler, err := r.FormFile("levelFiles")
     if err != nil {
         fmt.Println("Error Retrieving the File")
         fmt.Println(err)
@@ -132,7 +134,7 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func setupRoutes() {
-    http.HandleFunc("/upload", uploadFile)
+    http.HandleFunc("/level/", uploadFile)
 	http.HandleFunc("/auth/token",Login)
     http.ListenAndServe(":8080", nil)
 }
