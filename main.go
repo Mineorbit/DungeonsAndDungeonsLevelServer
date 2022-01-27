ee3647dfb826d7ade05568e330b08ef2e4cf784a
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-01-27 19:10:11 +0100

    fix

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ func getLevelList(w http.ResponseWriter, r *http.Request, proto_resp bool){
 		FullName: "TestLevel",
 	}
 	levels := LevelMetaDataList{
-		Levels: LevelMetaData[1] {level},
+		Levels: [1]*LevelMetaData {level},
 	}
     fmt.Fprintf(w, levels.String())
 }
